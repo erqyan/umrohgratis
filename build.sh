d29a136a518ea03180282b430b805648f2e7b0d8
erqyan <reegii90@gmail.com>
2026-07-17 00:57:07 +0700

    chore: build assets for Vercel

--- build.sh
+++ build.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# Install PHP & Composer
-apt-get update -qq
-apt-get install -y -qq php8.2-cli php8.2-mbstring php8.2-xml php8.2-curl php8.2-pgsql unzip > /dev/null 2>&1
-curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer > /dev/null 2>&1
+# Install Composer
+curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
 
-# Install dependencies
+# Install PHP dependencies
 composer install --no-dev --optimize-autoloader
+
+# Install & build frontend
 npm install
 npm run build
 
-# Cache Laravel
+# Cache Laravel config & routes
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
